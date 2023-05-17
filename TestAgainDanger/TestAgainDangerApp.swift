d73a03089e17eee33f580a57408f07e9ca6b0545
Curtis Scott <curtis.scott@gocity.com>
2023-05-17 21:23:43 +0100

    Initial Commit

---
+++ TestAgainDanger/TestAgainDangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAgainDangerApp.swift
+//  TestAgainDanger
+//
+//  Created by Curtis Scott on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAgainDangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
